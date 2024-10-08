48c9ac839f9a253c14eac7b1c86001bb41103da3
Roger Tuan <r.tuan@datocms.com>
2024-10-08 16:17:29 -0700

    Use PrimsReactRenderer instead of CodeMirror

--- src/ConfigScreen.tsx
+++ src/ConfigScreen.tsx
@@ -1,8 +1,9 @@
 import {Button, Canvas, Form, FormLabel, TextField} from 'datocms-react-ui';
 import {RenderConfigScreenCtx} from "datocms-plugin-sdk";
-import {useMemo, useState} from "react";
-import CodeMirror from '@uiw/react-codemirror';
-import {javascript} from '@codemirror/lang-javascript';
+import {Fragment, useCallback, useMemo, useRef, useState} from "react";
+import {Highlight, themes} from "prism-react-renderer";
+import {useEditable} from "use-editable";
+import './code-editor-styles.css';
 
 export type ValidatorFunction = (input: string) => boolean;
 
@@ -16,11 +17,29 @@ type PropTypes = {
     ctx: RenderConfigScreenCtx;
 };
 
+const validatorFnPlaceholder = `
+/**
+Provide a JS function that takes an "input" param and returns boolean true/false.
+Typescript is not supported.
+*/
+
+(input) => {
+    
+    // Return false to fail validation
+    if(input === 'invalid input') {
+        return false
+    }
+    
+    // Return true to pass validation
+    return true
+}
+`
+
 export default function ConfigScreen({ctx}: PropTypes) {
     const pluginParams = ctx.plugin.attributes.parameters as PluginParameters;
 
     const [customError, setCustomError] = useState<string>(pluginParams.customError ?? '');
-    const [validatorFn, setValidatorFn] = useState<string>(pluginParams.validatorFn ?? '');
+    const [validatorFn, setValidatorFn] = useState<string>(pluginParams.validatorFn ?? validatorFnPlaceholder);
 
     const onSave = () => {
         ctx.updatePluginParameters({
@@ -37,16 +56,35 @@ export default function ConfigScreen({ctx}: PropTypes) {
         return (pluginParams.validatorFn != validatorFn || pluginParams.customError != customError);
     }, [customError, validatorFn, pluginParams.validatorFn, pluginParams.customError]);
 
+    const editorRef = useRef(null);
+    const onEditableChange = useCallback((code: string) => {
+        setValidatorFn(code.slice(0, -1));
+    }, []);
+
+    useEditable(editorRef, onEditableChange, {
+        indentation: 2,
+    });
+
     return (
         <Canvas ctx={ctx}>
             <Form>
                 <FormLabel htmlFor={'codemirror'}>Custom validator</FormLabel>
-                <CodeMirror
-                    id='codemirror'
-                    extensions={[javascript()]}
-                    value={validatorFn}
-                    onChange={setValidatorFn}
-                />
+                <Highlight code={validatorFn} language="JavaScript" theme={themes.nightOwlLight}>
+                    {({style, tokens, getTokenProps}) => (
+                        <pre className={'code-editor'} style={style} ref={editorRef}>
+                      {tokens.map((line, i) => (
+                          <Fragment key={i}>
+                              {line
+                                  .filter((token) => !token.empty)
+                                  .map((token, key) => (
+                                      <span {...getTokenProps({token, key})} />
+                                  ))}
+                              {"\n"}
+                          </Fragment>
+                      ))}
+                    </pre>
+                    )}
+                </Highlight>
 
                 <TextField
                     id='customError'
